36f5255cd2a9309aa7a6f71db27667259f9070ea
Charmingdc <charmingdc002@gmail.com>
2024-10-01 17:55:39 +0100

    update AddEventListener.js

--- scripts/view/AddEventListener.js
+++ scripts/view/AddEventListener.js
@@ -8,6 +8,7 @@ export const AddEventListener = (elementId, type, callbackFunc) => {
   if (!element)  {
     console.error(`Element with the id of ${elementId.toUpperCase()} is not found`);
   } else {
+    
    // get element with the inputed id and attach an event listener to it.
      element.addEventListener(type, () => callbackFunc() );
   }
