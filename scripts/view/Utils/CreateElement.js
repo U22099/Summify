c3fdd5e38c981701a7778593efc096535ea27007
Charmingdc <charmingdc002@gmail.com>
2024-10-01 20:42:25 +0100

    update

---
+++ scripts/view/Utils/CreateElement.js
@@ -0,0 +1,8 @@
+export const CreateElement = (type) => {
+  
+  // create new element with the type argument and assign it to a variable 
+  const element = document.createElement(type)
+  
+  //return the variable
+  return element;
+}
