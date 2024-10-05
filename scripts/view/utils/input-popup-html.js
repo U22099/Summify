c3609432d62f78a8a22bb7fd7962a433151ae7da
U22099 <nifemiolaniyi4@gmail.com>
2024-10-05 06:25:02 +0100

    update

--- scripts/view/utils/input-popup-html.js
+++ scripts/view/utils/input-popup-html.js
@@ -12,19 +12,7 @@ export default `<div class="input-popup">
         </p>
       </nav>
       <main class="main">
-        <textarea type="text" placeholder="Paste in anything"></textarea>
-        <!--<label for="image-input">
-          <span>
-            Add Image
-          </span>
-          <input type="file" name="image" id="image-input" accept=".jpeg, .jpg, .png" hidden style="display: none;"/>
-        </label>-->
-        <!--<label for="file-input">
-          <span>
-            Add File
-          </span>
-          <input type="file" name="file" id="file-input" accept=".jpeg, .jpg, .png" hidden style="display: none;" />
-        </label>-->
+        <textarea type="text" placeholder="Paste in anything" id="text-input"></textarea>
       </main>
       <footer class="footer">
         <select name="length" id="length">
@@ -36,3 +24,27 @@ export default `<div class="input-popup">
       </footer>
     </div>
   </div>`;
+
+export const textInputHtml = `<textarea type="text" placeholder="Paste in anything" id="text-input"></textarea>`;
+
+export const imageInputHtml = (img) => `<label for="image-input">
+          <img src="${img}" alt="" />
+          <span>
+            Add Image
+          </span>
+          <input type="file" name="image" id="image-input" accept=".jpeg, .jpg, .png" hidden style="display: none;"/>
+        </label>`;
+
+export const fileInputHtml = (file, type) => `<label for="file-input">
+          <object data="${file}" type="${type}"></object>
+          <span>
+            Add File
+          </span>
+          <input type="file" name="file" id="file-input" accept=".jpeg, .jpg, .png" hidden style="display: none;" />
+        </label>`;
+export const fileInputHtml = (type, ext) => `<label for="file-input">
+          <span>
+            Add ${type}
+          </span>
+          <input type="file" name="file" id="file-input" accept="${ext}" hidden style="display: none;" />
+        </label>`
