c162d972698bc33d9e04548cb89bad88a628bd27
Daniel <156960927+U22099@users.noreply.github.com>
2024-10-06 01:36:54 +0000

    —-

--- scripts/controller/init.js
+++ scripts/controller/init.js
@@ -8,14 +8,16 @@ export default class Controller {
 
     this.view.runAddEventListener("open-nav", "click", () => {
       const sideBar = this.view.runGetElement(".side-bar")
-      sideBar.style.opacity = "1";
-      sideBar.classList.toggle(".animate-open-side-bar");
+      sideBar.classList.remove("animate-close-side-bar");
+      sideBar.classList.toggle("animate-open-side-bar");
+      sideBar.style.visibility = "visible";
     });
 
     this.view.runAddEventListener("close-nav", "click", () => {
       const sideBar = this.view.runGetElement(".side-bar")
-      sideBar.style.opacity = "0";
-      sideBar.classList.toggle(".animate-close-side-bar");
+      sideBar.classList.remove("animate-open-side-bar");
+      sideBar.classList.toggle("animate-close-side-bar");
+      setTimeout(()=>{sideBar.style.visibility = "hidden";},200);
     });
   }
 }
