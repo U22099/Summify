4d388c9f282931961f640eb76f167030534ec7cd
U22099 <nifemiolaniyi4@gmail.com>
2024-10-10 06:39:47 +0100

    update init.js

--- scripts/controller/init.js
+++ scripts/controller/init.js
@@ -129,7 +129,8 @@ export default class Controller {
       this.view.runInsertHTML("main", this.view.getResultPageHtml().resultPageHtml({
         icon,
         inputTitle,
-        action: this.action
+        action: `${this.action[0].toUpperCase()}${this.action.slice(1, this.
+        action.length - 1)}`
       }), "beforeend", false);
       this.popupClose(this.view.runGetElement(".input-popup"));
       //await this.model.init(this.action);
