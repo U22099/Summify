13eecd858125b1ab66ea1772b7b1edd3085f06b0
U22099 <nifemiolaniyi4@gmail.com>
2024-10-11 04:38:49 +0100

    update

--- scripts/model/utils/markdown-to-html.js
+++ scripts/model/utils/markdown-to-html.js
@@ -1,5 +1,24 @@
-import { parseMarkdownToHTML } from 'markdown-parser-html';
+import { Remarkable } from 'remarkable';
+import hljs from "highlight.js";
+import 'highlight.js/styles/github.css';
 
-export default function markdownToHtml(markdown){
-  return parseMarkdownToHTML(markdown);
+export default function markdownToHtml(markdown) {
+  const md = new Remarkable({
+    html: true,
+    xhtmlOut: true,
+    breaks: true,
+    typographer: true,
+    highlight: function(str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(str, { language: lang }).value;
+        } catch (e) { console.log(e) }
+      }
+      try {
+        return hljs.highlightAuto(str).value;
+      } catch (e) { console.log(e) }
+      return '';
+    }
+  });
+  return md.render(markdown);
 }
