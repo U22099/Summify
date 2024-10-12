e9ba2ac7d14c1b75ec5ba16131e3110c4e73c146
Daniel <156960927+U22099@users.noreply.github.com>
2024-10-12 23:06:34 +0000

    update

--- scripts/model/utils/text-to-pdf.js
+++ scripts/model/utils/text-to-pdf.js
@@ -1,6 +1,7 @@
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 export default function textToPdf(text, {title, subject, keywords}){
   const docDefinition = {
