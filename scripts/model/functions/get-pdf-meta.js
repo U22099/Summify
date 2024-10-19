c5c8e1555507dd28eb9f6e7fdf02d799cda4929c
Daniel <156960927+U22099@users.noreply.github.com>
2024-10-19 07:11:16 +0000

    update

--- scripts/model/functions/get-pdf-meta.js
+++ scripts/model/functions/get-pdf-meta.js
@@ -25,12 +25,12 @@ export default async function getPdfMeta(summary) {
     },
     systemInstruction: "You are an expert at meta object generation, good at giving concise and meaningful titles from a documents.",
   });
-  const prompt = "Generate an object of title from the document below: ";
+  const prompt = "Generate an object of title from the document below, don't add an extension: ";
 
   try {
     const generatedContent = await model.generateContent(prompt + "\n" + summary);
     const response = generatedContent.response.text();
-    return JSON.parse(response).title;
+    return JSON.parse(response).title.split(".")[0];
   } catch (e) {
     console.log(e)
     return false;
